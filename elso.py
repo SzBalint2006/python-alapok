11c98b490af0394fadf7d637488c3b1880d2a291
Pulykavok <szabobalint045@gmail.com>
2022-09-19 10:15:55 +0200

    üőpztr

--- elso.py
+++ elso.py
@@ -1,14 +1,19 @@
-from os import system
 
 system("cls")
 
+elsovaltozo = 12
 
+#string konkatenáció
+kiir_1 = "A változó értéke: " + str(elsovaltozo)
+print(kiir)
 
-elsoValtozo = 12
+# formattált sztring 1.verzió
+kiir_1 = "A változó értéke: {}" .format(elsovaltozo)
+print(kiir_1)
 
+# formattált sztring 2.verzió
+kiir_1 = f"A változó értéke:  {elsovaltozo}"
+print(kiir_1)
 
-print(elsoValtozo)
-
-elsoValtozo = "A 10_a a Legjobb"
-
-print(elsoValtozo)
+kiir_1 = "A változó értéke: {1}" .format(elsovaltozo)
+print(kiir_1)
